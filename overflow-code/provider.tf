6414eafb9461b60803c815f18aba2c948e9dc385
Willian Braga da Silva <neverping@gmail.com>
2026-02-11 22:41:14 +0100

    feat(workspace): new workspace
    See how it goes.

---
+++ overflow-code/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = ">= 7.17.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = "overflow-code-222"
+  region      = "us-central1"
+}
